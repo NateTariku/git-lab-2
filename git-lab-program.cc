23bffa952e98c4d724c8fd85daebf60b6e12c2c7
Natnael Tariku <nt863818@ohio.edu>
2019-01-23 21:29:34 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,21 @@
+
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nate Tariku
+ *        Date: January 23 2019
+ *   Description: This program is made at the computer lab.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout<< "Hello Git!!" <<endl;
+    return 0;
+}// main
